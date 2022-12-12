f19c83babddf48d9a675796f0c2b8e1306fb1383
MLanfear <maxjlanfear@gmail.com>
2022-12-12 00:59:09 -0800

    added css file code and starting component code--added my custom logo

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+import './App.css';
+
+function App() {
+
+  const [pageSelected, setPageSelected] = useState('about')
+
+  return (
+    <div className="App">
+      <Nav
+      pageSelected={pageSelected}
+      setPageSelected={setPageSelected}
+      />
+      {pageSelected==='about' && <About/>}
+      {pageSelected==='projects' && <Projects/>}
+      {pageSelected==='resume' && <Resume/>}
+      {pageSelected==='contact' && <Contact/>}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
